986c925905a15f911c39924a079d0f33bca8391a
Samuel Attard <samuel.r.attard@gmail.com>
2017-02-10 23:29:00 +1100

    Add usage examples to the templates page

--- src/components/TemplatesPage/ForgeTemplateBanner.jsx
+++ src/components/TemplatesPage/ForgeTemplateBanner.jsx
@@ -1,4 +1,7 @@
 import React, { PureComponent, PropTypes } from 'react';
+import CodeIcon from 'react-octicons-svg/dist/CodeIcon';
+
+import BashBlock, { Comment, Gap, Line } from '../BashBlock';
 
 import styles from './ForgeTemplateBanner.scss';
 
@@ -11,8 +14,51 @@ export default class ForgeTemplateBanner extends PureComponent {
     }),
   };
 
+  constructor(...args) {
+    super(...args);
+
+    this.state = {
+      sampleOpen: false,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('click', this._closeSample, true);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('click', this._closeSample);
+  }
+
+  _closeSample = (event) => {
+    if (this._sampleDiv && (this._sampleDiv.contains(event.target) || this._sampleDiv === event.target)) return;
+    if (!this.state.sampleOpen) return;
+    this.setState({
+      sampleOpen: false,
+    });
+    event.stopPropagation();
+  }
+
+  _refSample = (sampleDiv) => {
+    this._sampleDiv = sampleDiv;
+  }
+
+  _stopPropagation = (event) => {
+    if (this.state.sampleOpen) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+  _openSample = (event) => {
+    this.setState({
+      sampleOpen: true,
+    });
+  }
+
   render() {
-    const { template: { name, icon, description } } = this.props;
+    const { template: { key, name, icon, description } } = this.props;
+    const { sampleOpen } = this.state;
     const IconClass = icon;
 
     return (
@@ -21,9 +67,38 @@ export default class ForgeTemplateBanner extends PureComponent {
           <div className={styles.icon}><IconClass width={120} height={120} /></div>
         </div>
         <div className={styles.description}>
-          <h1 className={styles.name}>{name}</h1>
+          <h1 className={styles.name}>
+            <span className={styles.nameString}>{name}</span>
+            {
+              typeof key !== 'undefined' ?
+              (
+                <span className={`${styles.sampleIcon} ${sampleOpen ? styles.sampleIconActive : ''}`} onClick={this._openSample} title="Usage Example">
+                  <CodeIcon />
+                </span>
+              )
+              : null
+            }
+          </h1>
           {description}
         </div>
+        {
+          sampleOpen ?
+          (
+            <div className={styles.sample} onClick={this._stopPropagation} ref={this._refSample}>
+              <BashBlock>
+                <Line value="npm install -g electron-forge" />
+                <Gap />
+                <Comment value="Initialize a new project" />
+                <Line value={`electron-forge init my-new-project${key ? ` --template=${key}` : ''}`} />
+                <Gap />
+                <Comment value="Launch your app" />
+                <Line value="cd my-new-project" />
+                <Line value="electron-forge start" />
+              </BashBlock>
+            </div>
+          )
+          : null
+        }
       </div>
     );
   }
